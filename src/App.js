53ba0c7efda950d67b50480dfcd5fe32c02147d2
Steven DeTeso <sadeteso@gmail.com>
2023-03-09 20:32:23 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import Joke from './components/Joke'
+
+function App () {
+    return (
+    <div>
+        <Joke 
+        setup="Why did the tomato turn red?"
+        punchline="Because it saw the salad dressing!"
+        />
+        <Joke 
+        setup="Why was the math book sad?"
+        punchline="Because it had too many problems."
+        />
+        <Joke 
+        setup="What do you call a fake noodle?"
+        punchline="An impasta."
+        />
+        <Joke 
+        setup="Why don't scientists trust atoms?"
+        punchline="Because they make up everything."
+        />
     </div>
-  );
+    )
 }
 
-export default App;
+export default App
