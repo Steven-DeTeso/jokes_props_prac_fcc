dcb8714d5e84b6733c97a18c023f3092cb954c7b
Steven DeTeso <sadeteso@gmail.com>
2023-03-10 12:45:00 -0500

    map() practice to insert data as props

--- src/App.js
+++ src/App.js
@@ -1,28 +1,33 @@
 import React from 'react';
 
 import Joke from './components/Joke'
+import jokesData from './jokesData';
 
 function App () {
+    const jokeElements = jokesData.map(joke => {
+        return <Joke setup={joke.setup} punchline={joke.punchline}/>
+    })
     return (
     <div>
-        <Joke 
-        setup="Why did the tomato turn red?"
-        punchline="Because it saw the salad dressing!"
-        />
-        <Joke 
-        setup="Why was the math book sad?"
-        punchline="Because it had too many problems."
-        />
-        <Joke 
-        setup="What do you call a fake noodle?"
-        punchline="An impasta."
-        />
-        <Joke 
-        setup="Why don't scientists trust atoms?"
-        punchline="Because they make up everything."
-        />
+        {jokeElements}
     </div>
     )
 }
 
 export default App
+{/* <Joke 
+setup="Why did the tomato turn red?"
+punchline="Because it saw the salad dressing!"
+/>
+<Joke 
+setup="Why was the math book sad?"
+punchline="Because it had too many problems."
+/>
+<Joke 
+setup="What do you call a fake noodle?"
+punchline="An impasta."
+/>
+<Joke 
+setup="Why don't scientists trust atoms?"
+punchline="Because they make up everything."
+/> */}
